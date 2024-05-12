c6e244f668e22e6bc254752a0a31e9eb658c2d0d
betomark <almark16@gmail.com>
2024-05-12 03:32:31 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,20 +2,20 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-app = Flask(__name__)
-app.config.from_pyfile('config.py')  # Carga la configuración desde config.py
-db = SQLAlchemy(app)
+pericles = Flask(__name__)
+pericles.config.from_pyfile('config.py')  # Carga la configuración desde config.py
+db = SQLAlchemy(pericles)
 login_manager = LoginManager()
-login_manager.init_app(app)
+login_manager.init_app(pericles)
 
-from app import models  # Importa los modelos después de la inicialización de db
+from pericles import modelos  # Importa los modelos después de la inicialización de db
 
 @app.context_processor
 def inject_user():
     """Inyecta el usuario actual en el contexto de la plantilla"""
     return {'current_user': current_user}
 
-from app import routes  # Importa las rutas después de la inicialización de login_manager
+from app import rutas  # Importa las rutas después de la inicialización de login_manager
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    pericles.run(debug=True)
